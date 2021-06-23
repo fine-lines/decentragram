ce0cc53c25f3e55bc1dd0cf84901ddd424bd5f13
paul cooper <code@pcooper.de>
2021-06-23 04:34:42 +0200

    complete lesson

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,66 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.6;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    // Image DataStore
+    uint public imageCount = 0;
+    mapping(uint => Image) public images;
+
+    struct Image {
+        uint id;
+        string hash;
+        string description;
+        uint tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    )
+
+    // Create image
+    function uploadImage(string memory _imgHash, string memory _description) public {
+        // sanity checks
+        require(bytes(_imgHash) > 0);
+
+        imageCount = imageCount++;
+        images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+        // Trigger an event
+        emit ImageCreate(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // Tip Images
+    function tipImageOwner(uint _id) public payable {
+        // sanity checks
+        require(_id > 0, && _id <= imageCount);
+
+        Image memory _image = images[_id];
+        address payable _author = _image.author;
+        address(_author).transfer(msg.value);
+
+        // Increment tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+        // Update image
+        images[_id] = _image
+        // Trigger tip event
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _image.author)
+    }
 }
